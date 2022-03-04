d1d68d3dcde5a2b407db67ec2ed2c759d9bc18f1
Will Greenberg <greenberg.will@gmail.com>
2022-03-04 10:59:18 -0800

    add properties and constructor for order

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -4,6 +4,16 @@ namespace VendorTracker.Models
 {
   public class Order
   {
-    
+    public string Title { get; }
+    string Description { get; }
+    int Price { get; }
+    string Date { get; }
+    public Order( string title, string description, int price, string date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
+    }
   }
 }
