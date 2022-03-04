9180bd53d5e667d76933f57eb954d991aa126007
Will Greenberg <greenberg.will@gmail.com>
2022-03-04 11:03:37 -0800

    add set to order public properties

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -4,10 +4,10 @@ namespace VendorTracker.Models
 {
   public class Order
   {
-    public string Title { get; }
-    string Description { get; }
-    int Price { get; }
-    string Date { get; }
+    public string Title { get; set; }
+    string Description { get; set; }
+    int Price { get; set; }
+    string Date { get; set; }
     public Order( string title, string description, int price, string date)
     {
       Title = title;
