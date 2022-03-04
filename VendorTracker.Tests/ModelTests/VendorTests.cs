706a31519a0aee972f30f2e26ebd776a64096d4e
Will Greenberg <greenberg.will@gmail.com>
2022-03-04 08:19:44 -0800

    boilerplate for modeltests

---
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecordLibrary.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorTracker.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+  }
+}
