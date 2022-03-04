d1bcbec202b2a7648eb327790e75fcea675f6fd5
Will Greenberg <greenberg.will@gmail.com>
2022-03-04 09:38:21 -0800

    add instances list, add id property, add clearall method, add vendors to instances list

--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -4,14 +4,22 @@ namespace VendorTracker.Models
 {
   public class Vendor
   {
+    public static List<Vendor> _instances = new List<Vendor>{};
     public string Name { get; set;}
     public string Description { get; set; }
     public string Location { get; set;}
+    public int Id { get; }
     public Vendor(string name, string description, string location)
     {
       Name = name;
       Description = description;
       Location = location;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
