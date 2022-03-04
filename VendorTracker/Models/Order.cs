508c968ea5ee54f82179c7fed08a1ed913eb488d
Will Greenberg <greenberg.will@gmail.com>
2022-03-04 11:19:03 -0800

    add clearall method, add _instances list, add get all method to pass test

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -4,16 +4,28 @@ namespace VendorTracker.Models
 {
   public class Order
   {
+    private static List<Order> _instances = new List<Order> {};
     public string Title { get; set; }
-    string Description { get; set; }
-    int Price { get; set; }
-    string Date { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public string Date { get; set; }
+    public int Id { get; } 
     public Order( string title, string description, int price, string date)
     {
       Title = title;
       Description = description;
       Price = price;
       Date = date;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
