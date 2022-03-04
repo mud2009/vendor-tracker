10e95408e74628a4a313a9b0e7bec682f669e672
Will Greenberg <greenberg.will@gmail.com>
2022-03-04 14:59:01 -0800

    add httpget request for vendors id show

--- VendorTracker/Controllers/VendorsController.cs
+++ VendorTracker/Controllers/VendorsController.cs
@@ -24,5 +24,15 @@ namespace VendorTracker.Controllers
       Vendor newVendor = new Vendor(vendorName, vendorDesc, vendorLoc);
       return RedirectToAction("Index");
     }
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
   }
 }
