e7cc98830bd732cb8ff1a212a7c287f4dedbe2e6
Will Greenberg <greenberg.will@gmail.com>
2022-03-04 15:48:56 -0800

    switch vendors orders route to vendors controlloer

--- VendorTracker/Controllers/VendorsController.cs
+++ VendorTracker/Controllers/VendorsController.cs
@@ -34,5 +34,17 @@ namespace VendorTracker.Controllers
       model.Add("orders", vendorOrders);
       return View(model);
     }
+    [HttpPost("/vendors/{id}/orders")]
+    public ActionResult Create(string orderTitle, string orderDesc, int orderPric, string orderDat, int vendorId)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderTitle, orderDesc, orderPric, orderDat);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
   }
 }
