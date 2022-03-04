5c0a50be1e1bb04d2ee1e5dea389d355359c413b
Will Greenberg <greenberg.will@gmail.com>
2022-03-04 10:46:20 -0800

    add returns name test for order constructor, also add test set up

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -8,11 +8,29 @@ namespace VendorTracker.Tests
   [TestClass]
   public class OrderTests
   {
+    string title;
+    string description;
+    string price;
+    string date;
+    [TestInitialize]
+    public void Initialize()
+    {
+      title = "title";
+      description = "description";
+      date = "date";
+    }
     [TestMethod]
     public void OrderConstructor_CreatesOrderObject_Order()
     {
       Order newOrder = new Order();
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+    [TestMethod]
+    public void OrderConstructor_ReturnsName_String()
+    {
+      Order newOrder = new Order(title, description, date);
+      string result = newOrder.Title; 
+      Assert.AreEqual(title ,result);
     }
   }
 }
